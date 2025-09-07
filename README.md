96b3a5fac500ded7f45838413e6fbfbcd34449be
Bolazcoding <bolajiadelabu0@gmail.com>
2025-09-07 22:18:31 +0100

    New Features

---
+++ README.md
@@ -0,0 +1,55 @@
+# Frontend Mentor - Bookmark landing page solution
+
+This is a solution to the [Bookmark landing page challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/bookmark-landing-page-5d0b588a9edda32581d29158). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [Continued development](#continued-development)
+- [Author](#author)
+
+## Overview
+
+Users should be able to:
+
+- View the optimal layout for the interface depending on their device's screen size
+- See hover and focus states for all interactive elements on the page
+- Receive an error message when the newsletter form is submitted if:
+  - The input field is empty
+  - The email address is not formatted correctly
+
+### Screenshot
+
+- Desktop Preview
+
+![](./images/Desktop%20preview.png)
+
+- Mobile Preview
+
+![](./images/Mobile%20preview.png)
+
+### Links
+
+- Solution URL: https://github.com/Bolazcoding/bookmark-landing-page
+- Live Site URL: https://bookmark-landing-page-tawny-two.vercel.app/
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- JavaScript
+
+### Continued development
+
+This project tested my skills but I'm looking forward to more projects to build in the future.
+
+## Author
+
+- Website - [my github](https://github.com/Bolazcoding)
+- Frontend Mentor - [@myusername](https://www.frontendmentor.io/profile/Bolazcoding)
+- Twitter - [@myusername](https://x.com/_joseDev_)
--- script.js
+++ script.js
@@ -1,110 +1,150 @@
-"use strict";
+'use strict';
 
-const openMenu = document.querySelector(".open-menu");
-const closeMenu = document.querySelector(".close-menu");
-const openMobileNav = document.querySelector(".sidebar-header");
-const closeDesNav = document.querySelector(".bookmark-logo");
-const navLink = document.querySelectorAll(".nav-list");
-const contentheader1 = document.querySelector(".content-header1");
-const contentheader2 = document.querySelector(".content-header2");
-const contentheader3 = document.querySelector(".content-header3");
-const content1El = document.querySelector(".content1");
-const content2El = document.querySelector(".content2");
-const content3El = document.querySelector(".content3");
-const contentFeatures1 = document.querySelector(".features-details1");
-const contentFeatures2 = document.querySelector(".features-details2");
-const contentFeatures3 = document.querySelector(".features-details3");
+const openMenu = document.querySelector('.open-menu');
+const closeMenu = document.querySelector('.close-menu');
+const openMobileNav = document.querySelector('.sidebar-header');
+const closeDesNav = document.querySelector('.bookmark-logo');
+const navLink = document.querySelectorAll('.nav-list');
+const contentheader1 = document.querySelector('.content-header1');
+const contentheader2 = document.querySelector('.content-header2');
+const contentheader3 = document.querySelector('.content-header3');
+const content1El = document.querySelector('.content1');
+const content2El = document.querySelector('.content2');
+const content3El = document.querySelector('.content3');
+const contentFeatures1 = document.querySelector('.features-details1');
+const contentFeatures2 = document.querySelector('.features-details2');
+const contentFeatures3 = document.querySelector('.features-details3');
 
-const bg_animation = document.querySelector(".features");
+const bg_animation = document.querySelector('.features');
 
 // Mobile Navigation
-openMenu.addEventListener("click", function () {
-  closeDesNav.style.display = "none";
-  openMenu.style.display = "none";
-  openMobileNav.classList.remove("hidden-nav");
+openMenu.addEventListener('click', function () {
+  closeDesNav.style.display = 'none';
+  openMenu.style.display = 'none';
+  openMobileNav.classList.remove('hidden-nav');
 });
 
-closeMenu.addEventListener("click", function () {
-  closeDesNav.style.display = "block";
-  openMenu.style.display = "block";
-  openMobileNav.classList.add("hidden-nav");
+closeMenu.addEventListener('click', function () {
+  closeDesNav.style.display = 'block';
+  openMenu.style.display = 'block';
+  openMobileNav.classList.add('hidden-nav');
 });
 
 navLink.forEach(function (item) {
-  item.addEventListener("click", function () {
-    closeDesNav.style.display = "block";
-    openMenu.style.display = "block";
-    openMobileNav.classList.add("hidden-nav");
+  item.addEventListener('click', function () {
+    closeDesNav.style.display = 'block';
+    openMenu.style.display = 'block';
+    openMobileNav.classList.add('hidden-nav');
   });
 });
 
 // Features Section toggle on and off
-contentheader1.addEventListener("click", function () {
-  content1El.style.setProperty("--before-display", "block");
-  content2El.style.setProperty("--before-display", "none");
-  content3El.style.setProperty("--before-display", "none");
+contentheader1.addEventListener('click', function () {
+  content1El.style.setProperty('--before-display', 'block');
+  content2El.style.setProperty('--before-display', 'none');
+  content3El.style.setProperty('--before-display', 'none');
 
-  contentFeatures1.style.display = "flex";
-  contentFeatures2.style.display = "none";
-  contentFeatures3.style.display = "none";
+  contentFeatures1.style.display = 'flex';
+  contentFeatures2.style.display = 'none';
+  contentFeatures3.style.display = 'none';
 });
 
-contentheader2.addEventListener("click", function () {
-  content2El.style.setProperty("--before-display", "block");
-  content1El.style.setProperty("--before-display", "none");
-  content3El.style.setProperty("--before-display", "none");
+contentheader2.addEventListener('click', function () {
+  content2El.style.setProperty('--before-display', 'block');
+  content1El.style.setProperty('--before-display', 'none');
+  content3El.style.setProperty('--before-display', 'none');
 
-  contentFeatures1.style.display = "none";
-  contentFeatures2.style.display = "flex";
-  contentFeatures3.style.display = "none";
+  contentFeatures1.style.display = 'none';
+  contentFeatures2.style.display = 'flex';
+  contentFeatures3.style.display = 'none';
 
-  bg_animation.style.setProperty("--before-display", "none");
-  bg_animation.style.setProperty("--before-display", "block");
+  bg_animation.style.setProperty('--before-display', 'none');
+  bg_animation.style.setProperty('--before-display', 'block');
 });
 
-contentheader3.addEventListener("click", function () {
-  content3El.style.setProperty("--before-display", "block");
-  content1El.style.setProperty("--before-display", "none");
-  content2El.style.setProperty("--before-display", "none");
+contentheader3.addEventListener('click', function () {
+  content3El.style.setProperty('--before-display', 'block');
+  content1El.style.setProperty('--before-display', 'none');
+  content2El.style.setProperty('--before-display', 'none');
 
-  contentFeatures1.style.display = "none";
-  contentFeatures2.style.display = "none";
-  contentFeatures3.style.display = "flex";
+  contentFeatures1.style.display = 'none';
+  contentFeatures2.style.display = 'none';
+  contentFeatures3.style.display = 'flex';
 });
 
 // FAQ Function;
-const firstAnswer = document.querySelector(".answer");
-const questions = document.querySelectorAll(".faq-quest");
-const closeAnswer = document.querySelectorAll(".close-faq");
-const switch_icon = document.querySelectorAll(".open-faq");
+const firstAnswer = document.querySelector('.answer');
+const questions = document.querySelectorAll('.faq-quest');
+const closeAnswer = document.querySelectorAll('.close-faq');
+const switch_icon = document.querySelectorAll('.open-faq');
 
 questions.forEach((question, i) => {
-  question.addEventListener("click", () => {
+  question.addEventListener('click', () => {
     const answer = question.nextElementSibling;
 
-    if (answer.style.display === "block") {
+    if (answer.style.display === 'block') {
       // close it
-      answer.style.display = "none";
-      switch_icon[i].style.display = "block";
-      closeAnswer[i].style.display = "none";
+      answer.style.display = 'none';
+      switch_icon[i].style.display = 'block';
+      closeAnswer[i].style.display = 'none';
     } else {
       // reset everything
-      document.querySelectorAll(".answer").forEach((ans) => {
-        ans.style.display = "none";
+      document.querySelectorAll('.answer').forEach(ans => {
+        ans.style.display = 'none';
       });
 
-      document.querySelectorAll(".open-faq").forEach((icon) => {
-        icon.style.display = "block";
+      document.querySelectorAll('.open-faq').forEach(icon => {
+        icon.style.display = 'block';
       });
 
-      document.querySelectorAll(".close-faq").forEach((icon) => {
-        icon.style.display = "none";
+      document.querySelectorAll('.close-faq').forEach(icon => {
+        icon.style.display = 'none';
       });
 
       // open the clicked one
-      answer.style.display = "block";
-      switch_icon[i].style.display = "none";
-      closeAnswer[i].style.display = "block";
+      answer.style.display = 'block';
+      switch_icon[i].style.display = 'none';
+      closeAnswer[i].style.display = 'block';
     }
   });
+});
+
+// Email Validation
+document.getElementById('email-form').addEventListener('submit', function (e) {
+  e.preventDefault(); // stop reload
+  const errorMessage = document.querySelector('.email-error-message');
+  const errorImage = document.querySelector('.error-img');
+  const emailInput = document.querySelector('.email');
+  let email = document.getElementById('email').value;
+  // console.log('Submitted email:', email);
+  // console.log(errorMessage);
+
+  if (!email) {
+    errorMessage.style.display = 'block';
+    errorImage.style.display = 'block';
+    errorMessage.textContent = `Whoops, make sure it's not empty`;
+    emailInput.classList.add('email-error');
+    emailInput.classList.remove('email-success');
+    errorMessage.style.backgroundColor = 'hsl(0, 94%, 66%)';
+
+    return;
+  }
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (!emailRegex.test(email)) {
+    errorMessage.style.display = 'block';
+    errorImage.style.display = 'block';
+    errorMessage.textContent = `Whoops, make sure it's an email`;
+    emailInput.classList.add('email-error');
+    emailInput.classList.remove('email-success');
+    errorMessage.style.backgroundColor = 'hsl(0, 94%, 66%)';
+  } else {
+    errorImage.style.display = 'none';
+    errorMessage.style.display = 'block';
+    errorMessage.textContent = '✅ Success! Your email has been submitted.';
+    errorMessage.style.backgroundColor = 'green';
+    emailInput.classList.remove('email-error');
+    emailInput.classList.add('email-success');
+  }
 });
